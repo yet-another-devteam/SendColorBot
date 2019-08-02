d577d31f4399ac2dfdf6d19d09c76cdce1dd9b04
Dubzer <dubzer@ya.ru>
2019-08-03 00:44:03 +0300

    Finished InlineCardProcessor

--- SendColorBot/InlineCardProcessor.cs
+++ SendColorBot/InlineCardProcessor.cs
@@ -1,21 +1,37 @@
 using SixLabors.ImageSharp.PixelFormats;
+using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.InlineQueryResults;
 
 namespace SendColorBot
 {
     public class InlineCardProcessor
     {
-        private ImageGenerator imageGenerator;
+        private readonly ImageGenerator imageGenerator;
         
         public InlineCardProcessor()
         {
             imageGenerator = new ImageGenerator(Configuration.Root["tokens:fileserver-path"], Configuration.Root["tokens:fileserver-ip"]);    
         }
         
-        public InlineQueryResultPhoto ProccessInlineCard(byte id, Rgba32 color, string colorSpaceName)
+        /// <param name="id">Card ID</param>
+        /// <param name="color">With this color ImageGenerator will generate picture</param>
+        /// <param name="colorSpaceName">Color space that used for this color</param>
+        public InlineQueryResultArticle ProcessInlineCard(byte id, Rgba32 color, string colorSpaceName)
         {
+            // Generates picture with color
             string photoUrl = imageGenerator.Generate(color);
-            return new InlineQueryResultPhoto(id.ToString(), photoUrl, photoUrl);
+
+            // Content for ResultArticle
+            InputTextMessageContent messageContent = new InputTextMessageContent($"[ ]({photoUrl})")
+            {
+                ParseMode = ParseMode.Markdown, DisableWebPagePreview = false,
+            };
+
+            return new InlineQueryResultArticle(id.ToString(), colorSpaceName, messageContent)
+            {
+                HideUrl = true, 
+                ThumbUrl = photoUrl
+            };
         }
     }
 }   
