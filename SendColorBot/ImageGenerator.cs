dd75805a13331e5e8803d9d524796d82a46d0c2a
Dubzer <Dubzer@users.noreply.github.com>
2019-09-29 00:59:21 +0300

    Fixed bug with color

--- SendColorBot/ImageGenerator.cs
+++ SendColorBot/ImageGenerator.cs
@@ -13,8 +13,8 @@ namespace SendColorBot
         }
         
         public string GetLink(Rgba32 color)
-        {
-            return $"{_domain}?width=250&height=250&color={Convert.ToDecimal(color.R)}{Convert.ToDecimal(color.G)}{Convert.ToDecimal(color.B)}";
+        { 
+            return $"{_domain}?width=250&height=250&color={color.ToHex()}";
         }
     }
 }
